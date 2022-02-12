818b7e2bd9563746d97fb07aaa1ee26cb8668a61
Mrin111 <98249230+Mrin111@users.noreply.github.com>
2022-02-12 11:04:51 +0530

    Day 4 Assignment Submitted

---
+++ Day4/day4.js
@@ -0,0 +1,40 @@
+var avengers=[
+    {
+        name:"Thor",
+        age:1500,
+        weapon:"strombreaker",
+        Realname:"Chris Hemsworth"
+    },
+    {
+        name:"Ironman",
+        age:34,
+        weapon:"Armour",
+        Realname:"Anthony Edward Stark"
+    }
+   
+]
+var students=[
+    {
+     name:"Joy",
+     rollno:22,
+     age:14,
+     fathername:"Amalash"
+    },
+    {
+        name:"Bijay",
+        rollno:10,
+        age:14,
+        fathername:"Poritosh"  
+    }
+]
+
+
+for(var i=0;i<avengers.length;i++)
+{
+    console.log(avengers[i].age);
+}
+
+for(var a=0;a<students.length;a++)
+{
+    console.log(students[a].rollno);
+}
